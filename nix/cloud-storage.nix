b7075172a8e91b0b4452f88edd7a6958fff8a077
KevinLau <151832546+KevinLaucn@users.noreply.github.com>
2026-09-06 14:23:53 +0800

    build: export self-host email service leaf packages

--- nix/cloud-storage.nix
+++ nix/cloud-storage.nix
@@ -1302,6 +1302,7 @@
       // dopplerConfigBinPackages
       // deployServiceBinaryPackages
       // deployLambdaPackages
+      // selfHostEmailBinaryPackages
       // pkgs.lib.optionalAttrs isLinux {
         local-stack-binaries = localStackBinaries;
         self-host-email-binaries = selfHostEmailBinaries;
